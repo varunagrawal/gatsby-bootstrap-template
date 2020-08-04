807ac6b7cbcf1daf3aad4ed7addf6311d00bae04
Varun Agrawal <varagrawal@gmail.com>
2020-08-04 15:10:44 -0500

    Gatsby files

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby Bootstrap Template",
+  },
+  pathPrefix: `/gatsby-bootstrap-template`,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Gatsby Bootstrap Template",
+        short_name: "gatsby-bootstrap-template",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "static/favicon.ico", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+  ],
+}
---
+++ gatsby-ssr.js
@@ -0,0 +1,27 @@
+const React = require("react")
+const { Helmet } = require("react-helmet")
+
+exports.onRenderBody = (
+  {
+    setHeadComponents,
+    setHtmlAttributes,
+    setBodyAttributes,
+    setPreBodyComponents,
+    setPostBodyComponents,
+  },
+  pluginOptions
+) => {
+  const helmet = Helmet.renderStatic()
+  setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ])
+  setPreBodyComponents([])
+  setPostBodyComponents([])
+}
